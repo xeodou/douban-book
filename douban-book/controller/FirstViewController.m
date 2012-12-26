36081a2548a061427c4143ebfcf9f29b572f029e
xeodou <xeodou@gmail.com>
2012-12-26 23:21:49 +0800

    commit

---
+++ douban-book/controller/FirstViewController.m
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.m
+//  douban-book
+//
+//  Created by xeodou on 12-12-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
