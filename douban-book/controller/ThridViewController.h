d6dca46575b59ca072b671457b51f8d7071f671c
xeodou <xeodou@gmail.com>
2012-12-26 23:21:19 +0800

    add third controller 

---
+++ douban-book/controller/ThridViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThridViewController.h
+//  douban-book
+//
+//  Created by xeodou on 12-12-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThridViewController : UIViewController
+
+@end
---
+++ douban-book/controller/ThridViewController.m
@@ -0,0 +1,43 @@
+//
+//  ThridViewController.m
+//  douban-book
+//
+//  Created by xeodou on 12-12-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ThridViewController.h"
+
+@interface ThridViewController ()
+
+@end
+
+@implementation ThridViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
